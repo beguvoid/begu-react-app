74fe5a7e8d009751c730c51317d81fd8dc8f24f7
beguvoid <bilguunsoninbayar@gmail.com>
2020-07-28 00:26:45 +0800

    Second commit

---
+++ precache-manifest.168289a78fc1114326f7aec73669c4f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a68a0dc8bed1fdba5d4365677623b58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42b6d060b933cb3957c1",
+    "url": "/static/css/main.9a6bfc44.chunk.css"
+  },
+  {
+    "revision": "ed443b3ac0a5d282fd14",
+    "url": "/static/js/2.f12b28f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f12b28f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b6d060b933cb3957c1",
+    "url": "/static/js/main.efee38cb.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "4dadea97dfc03422825671a3504b6d56",
+    "url": "/static/media/coin.4dadea97.gif"
+  },
+  {
+    "revision": "865242aa92493267d955b6f995bc516c",
+    "url": "/static/media/still.865242aa.jpg"
+  }
+]);
